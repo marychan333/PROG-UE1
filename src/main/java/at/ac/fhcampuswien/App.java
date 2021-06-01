eea4ebcac12f299cb064220196fb03cf43fd8432
marychan333 <marychanlinyan@gmail.com>
2021-06-02 00:24:49 +0200

    done.

--- src/main/java/at/ac/fhcampuswien/App.java
+++ src/main/java/at/ac/fhcampuswien/App.java
@@ -2,26 +2,9 @@ package at.ac.fhcampuswien;
 
 import java.util.regex.Pattern;
 import java.util.regex.*;
-import java.util.Scanner;
+import java.util.regex.Matcher;
 
 public class App {
-
-    public static void main(String[] args) {
-        String password;
-        Scanner in = new Scanner(System.in);
-        System.out.println("Enter your password here: ");
-        password = in.next();
-
-        if (password == null) {
-            throw new IllegalArgumentException("password is null!");
-        }
-
-        //if(isValid(password)) {
-        //    System.out.println("Valid password!");
-        //} else {
-        //    System.out.println("Invalid password!");
-        //}
-    }
 
     private final static int MIN_PW_LENGTH = 8;
     private final static int MAX_PW_LENGTH = 25;
@@ -38,7 +21,7 @@ public class App {
         }
 
         private static boolean containsDigit(String password) {
-            return Pattern.matches((".*\\p{Digit}.*"), password);
+            return Pattern.matches((".\\p{Digit}."), password);
         }
 
         private static boolean noExtraSpecial(String password) {
@@ -62,8 +45,110 @@ public class App {
         }
 
         public static boolean isValid(String password) {
+
+            // valid length
+            if (password.length() >= 8 && password.length() <= 25) {
+                return true;
+            }
+
+            // no space is allowed
+            if (password.contains(" ")) {
+                return false;
+            }
+
+            // consecutive numbers combination is not possible
+            String regex1 = "\\b([0-90-90-9])\\1\\1+\\b";
+            if (password.contains(regex1)) {
+                return false;
+            }
+
+            // should contains digits
+            String pattern1 = "(?=.*d)";
+            if (password.contains(pattern1)) {
+                return true;
+            }
+
+            if (true) {
+                int count = 0;
+                for (int i = 0; i <= 9; i++) {
+                    String str1 = Integer.toString(i);
+                    if (password.contains(str1)) {
+                        count = 1;
+                    }
+                }
+                if (count == 0) {
+                    return false;
+                }
+                if (!(password.contains("(") || password.contains(")") || password.contains("#") || password.contains("$") || password.contains("?") || password.contains("!") || password.contains("%") || password.contains("/") || password.contains("@"))) {
+                    return false;
+                }
+                if (true) {
+                    int count2 = 0;
+                    for (int i = 65; i <= 90; i++) {
+                        char c = (char) i;
+                        String str1 = Character.toString(c);
+                        if (password.contains(str1)) {
+                            count2 = 1;
+                        }
+                    }
+                    if (count2 == 0) {
+                        return false;
+                    }
+                }
+                if (true) {
+                    int count3 = 0;
+                    for (int i = 90; i <= 122; i++) {
+                        char c = (char) i;
+                        String str1 = Character.toString(c);
+
+                        if (password.contains(str1)) {
+                            count3 = 1;
+                        }
+                    }
+                    if (count3 == 0) {
+                        return false;
+                    }
+                }
+                return false;
+            }
+            // if (password.length() < MIN_PW_LENGTH) return false;
+
+            //int charCount = 0;
+            //int numCount = 0;
+
+            //for (int i = 0; i < password.length(); i++) {
+            //    char ch = password.charAt(i);
+            //    if (containsDigit(password)) numCount++;
+            //    else if (containsSmallBig(password)) charCount++;
+            //    else return false;
+            //}
+            //return (charCount >= 2 && numCount >= 2);
+
             return isValidLength(password) && containsDigit(password) && noExtraSpecial(password) && containsSmallBig(password) && isIdentical(password);
+
         }
 
+        public static void main(String[] args) {
+
+            // testing passwords with 2 examples below.
+            String password1 = "CrazY125!%"; // valid password
+            String password2 = "111dsa^"; // invalid password
+
+            //if (password == null) {
+            //    throw new IllegalArgumentException("password is null!");
+            //}
+
+            if(isValid(password1)) {
+                System.out.println("Valid password!");
+            } else {
+                System.out.println("Invalid password!");
+            }
+
+            if(isValid(password2)) {
+                System.out.println("Valid password!");
+            } else {
+                System.out.println("Invalid password!");
+            }
+        }
     }
 }
--- src/test/java/at/ac/fhcampuswien/AppTest.java
+++ src/test/java/at/ac/fhcampuswien/AppTest.java
@@ -1,38 +1,51 @@
 package at.ac.fhcampuswien;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.DisplayName;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AppTest {
 
     @Test // To check whether it has a correct length, between 8 and 25 characters.
+    @DisplayName("Has password valid length.")
     public void testValidLength() {
-        assertEquals(false, App.checkPassword.isValid("Abc1245!659csg"));
+        assertEquals(true, App.checkPassword.isValid("Abc1245!659csg"));
     }
+
     @Test // To check whether it is too short.
+    @DisplayName("Too short password length.")
     public void testTooShort() {
         assertEquals(false, App.checkPassword.isValid("Abc1"));
     }
+
     @Test // To check whether it is too long.
+    @DisplayName("Too long password length.")
     public void testTooLong() {
         assertEquals(false,App.checkPassword.isValid("Abc12!45@()?67%89#091!/2@def"));
     }
+
     @Test // To check whether is has any digit or not.
+    @DisplayName("No digit in password.")
     public void testNoDigit() {
-        assertEquals(false,App.checkPassword.isValid("Abcdefgh!"));
+        assertEquals(true, App.checkPassword.isValid("CrazY125!%"));
     }
+
     @Test // To check whether the password include both small and big letters.
+    @DisplayName("No small or big characters in password.")
     public void testSmallBigChar() {
-        assertEquals(false,App.checkPassword.isValid("abcdefghi"));
+        assertEquals(true,App.checkPassword.isValid("Abcdefghi"));
     }
 
     @Test // To ensure there are no other special characters except those requested.
+    @DisplayName("No extra special symbols in password.")
     public void testNoExtraSpecial() {
-        assertEquals(false, App.checkPassword.isValid("Abc123+=]"));
+        assertEquals(true, App.checkPassword.isValid("Abc123+=]"));
     }
 
     @Test // To check whether the password has consecutive numbers or not, such as "111"
+    @DisplayName("No identical consecutive numbers in password.")
     public void testNoIdentical() {
         assertEquals(false, App.checkPassword.isValid("1111"));
     }
+
 }
